c9a843b6e54faf0fcea338859dc61040d99e4aa2
Zhc <zhc@qq.com>
2018-09-25 21:53:36 +0800

    完成遍历省市县三级列表的功能

---
+++ app/src/main/java/com/zhc/hefengweather/util/HttpUtil.java
@@ -0,0 +1,19 @@
+package com.zhc.hefengweather.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by zhc on 2018/9/25.
+ * 与服务器交互
+ */
+
+public class HttpUtil {
+
+    public static void sendOKHttpRequest(String address,okhttp3.Callback callback){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+
+}
